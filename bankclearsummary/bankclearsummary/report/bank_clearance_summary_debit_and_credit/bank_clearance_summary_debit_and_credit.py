3c817116d301ed033b45ab3c4f7b0d36ff3466cf
ahmadragheb <ahmedragheb75@gmail.com>
2019-02-11 15:24:31 +0200

    refactor2

--- bankclearsummary/bankclearsummary/report/bank_clearance_summary_debit_and_credit/bank_clearance_summary_debit_and_credit.py
+++ bankclearsummary/bankclearsummary/report/bank_clearance_summary_debit_and_credit/bank_clearance_summary_debit_and_credit.py
@@ -55,7 +55,7 @@ def get_entries(filters):
 
 
 	payment_entries =  frappe.db.sql("""select "Payment Entry", name, posting_date,
-		reference_no, clearance_date, party, paid_amount, received_amount,
+		reference_no, clearance_date, party, if(paid_from=%(account)s, paid_amount, 0),  if(paid_from!=%(account)s, received_amount, 0),
 		 if(paid_from=%(account)s, paid_amount, received_amount)
 		from `tabPayment Entry`
 		where docstatus=1 and (paid_from = %(account)s or paid_to = %(account)s) {0}
