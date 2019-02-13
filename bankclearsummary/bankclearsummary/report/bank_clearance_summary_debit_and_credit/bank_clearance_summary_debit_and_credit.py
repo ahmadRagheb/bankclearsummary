521d5319dae0f71c174c5b9db24c203beeec884a
ahmadragheb <ahmedragheb75@gmail.com>
2019-02-13 20:46:57 +0200

    fix

--- bankclearsummary/bankclearsummary/report/bank_clearance_summary_debit_and_credit/bank_clearance_summary_debit_and_credit.py
+++ bankclearsummary/bankclearsummary/report/bank_clearance_summary_debit_and_credit/bank_clearance_summary_debit_and_credit.py
@@ -10,9 +10,12 @@ def execute(filters=None):
 	if not filters: filters = {}
 
 	columns = get_columns()
-	data= get_entries(filters)
+	data, total, total2, total3= get_entries(filters)
 	# total_raw = [u'Total', None, None, None, None, None, total_credit, total_debit,general_total]
 	# data.append(total_raw)
+	# frappe.msgprint(str(total))
+	total_raw = [u'Total', None, None, None, None, None, total3, total2, total]
+	data.append(total_raw)
 	return columns, data
 
 
@@ -27,8 +30,6 @@ def get_columns():
 		_("Debit") + ":Currency:120",
 		_("Credit") + ":Currency:120",
 		_("Amount") + ":Currency:120",
-
-		# _("Total"),
 	]
 
 def get_conditions(filters):
@@ -48,12 +49,6 @@ def get_entries(filters):
 		where jvd.parent = jv.name and jv.docstatus=1 and jvd.account = %(account)s {0}
 		order by posting_date DESC, jv.name DESC""".format(conditions), filters, as_list=1)
 
-	# total_journal =  frappe.db.sql("""select sum(jvd.debit - jvd.credit)
-	# 	from `tabJournal Entry Account` jvd, `tabJournal Entry` jv
-	# 	where jvd.parent = jv.name and jv.docstatus=1 and jvd.account = %(account)s {0}
-	# 	order by posting_date DESC, jv.name DESC""".format(conditions), filters, as_list=1)
-
-
 	payment_entries =  frappe.db.sql("""select "Payment Entry", name, posting_date,
 		reference_no, clearance_date, party, if(paid_from!=%(account)s, received_amount, 0),if(paid_from=%(account)s, -paid_amount, 0),
 		 if(paid_from=%(account)s, -paid_amount, received_amount)
@@ -61,27 +56,16 @@ def get_entries(filters):
 		where docstatus=1 and (paid_from = %(account)s or paid_to = %(account)s) {0}
 		order by posting_date DESC, name DESC""".format(conditions), filters, as_list=1)
 
-	# total_payment =  frappe.db.sql("""select sum(if(paid_from=%(account)s, paid_amount, received_amount))
-	# 	from `tabPayment Entry` where docstatus=1 and (paid_from = %(account)s or paid_to = %(account)s) {0}
-	# 	order by posting_date DESC, name DESC""".format(conditions), filters, as_list=1)
+	result = sorted(journal_entries + payment_entries, key=lambda k: k[2] or getdate(nowdate()))
+	cleand = [flt(x[-1]) for x in result]
+	cleand_2 = [flt(x[-2]) for x in result]
+	cleand_3 = [flt(x[-3]) for x in result]
+	total = sum(cleand)
+	total2 = sum(cleand_2)
+	total3 = sum(cleand_3)
 
-	# journal =total_journal[0]
-	# payment =total_payment[0]
+	for i, record in enumerate(result):
+		del result[i][-1]
+		result[i].append(0)
 
-	# sub_journal = flt(journal[0])-flt(journal[1])
-	# sub_payment = flt(payment[0])-flt(payment[1])
-
-	# total_credit = flt(journal[0])+flt(payment[0])
-	# total_debit = flt(journal[0])+flt(payment[1])
-
-	# frappe.throw(str(sub_journal)+str(sub_payment))
-
-	# general_total = abs(flt(sub_journal)+flt(sub_payment))
-
-	# credit = flt(total_credit) + flt(received_amount)
-	# debit = flt(total_debit) + flt(paid_amount)
-
-	# total = credit-debit 
-
-	return sorted(journal_entries + payment_entries, key=lambda k: k[2] or getdate(nowdate()))
-	# general_total,total_credit,total_debit
+	return result,total,total2,total3
